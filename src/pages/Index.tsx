4f09ec93d5f78d0af4b945093cf9e07979e3094d
hatoma daiya <phaosdove@gmail.com>
2024-12-28 15:52:16 +0900

    地図を更新

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -99,6 +99,17 @@ const Index = () => {
                     沖縄県石垣市平得346 OK商店<br />
                     平得東バス停から徒歩3分
                   </p>
+                  <div className="mt-4 aspect-video w-full rounded-lg overflow-hidden shadow-md">
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3635.242290674521!2d124.1774608!3d24.3380513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34600a98cd9743af%3A0x16931829c48aa27f!2z77yv77yr5ZWG5bqX!5e0!3m2!1sja!2sjp!4v1735368321873!5m2!1sja!2sjp"
+                      width="100%"
+                      height="100%"
+                      style={{ border: 0 }}
+                      allowFullScreen
+                      loading="lazy"
+                      referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  </div>
                 </div>
                 <div className="space-y-2">
                   <h3 className="text-2xl font-semibold">営業時間</h3>
